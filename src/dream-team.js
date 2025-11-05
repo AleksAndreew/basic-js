7ee2b7a0cb397802972dd96c19a3c2b1abb51501
AleksAndreew <andreew.work@mail.ru>
2025-11-05 14:31:09 +0300

    8

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../lib');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+let arr =[];
+  members.forEach(elem =>{
+if (typeof(elem) === "string")
+{
+let a = elem.trimStart().toUpperCase();
+arr.push(a[0])
+}
+})
+return arr.sort().join('');
 }
 
 module.exports = {
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,9 +16,27 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function repeater(/* str, options */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+options.addition = options.addition !== undefined ? String(options.addition) : '';
+if(!options.repeatTimes) options.repeatTimes = 1;
+if(!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+if(!options.separator) options.separator = '+';
+if(!options.additionSeparator) options.additionSeparator = '|';
+
+function addition (name , separator, times ){
+let arr =[];
+arr = Array(times).fill(name);
+let string = arr.join(separator);
+return string;
+}
+
+let result = '';
+
+let additionalString = addition(options.addition, options.additionSeparator, options.additionRepeatTimes);
+let arr1=  Array(options.repeatTimes).fill(str + additionalString);
+result = arr1.join(options.separator);
+
+return result;
 }
 
 module.exports = {
